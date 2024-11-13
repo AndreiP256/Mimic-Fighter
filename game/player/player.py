60f387f96895504c947fa382658a51694fa8b07c
Lucci228 <151786062+Lucci228@users.noreply.github.com>
2024-11-13 20:23:01 +0200

    Added moving animations

--- game/player/player.py
+++ game/player/player.py
@@ -1,4 +1,6 @@
 # game/player.py
+import string
+
 import pygame
 from matplotlib.backend_bases import cursors
 from overrides import override
@@ -8,21 +10,23 @@ from game.sprites.sprite import Spritesheet
 
 
 class Player(AnimatedSprite):
-    def __init__(self, spritesheet , frame_width , frame_height, x, y, speed,
-                  scale=1, frame_rate=30):
+    def __init__(self, spritesheet, frame_width: int, frame_height: int, x: int, y: int, speed: int,
+                 scale: object = 1, frame_rate: int = 30, health: object = 100, attack_power: object = 10):
         pygame.sprite.Sprite.__init__(self)
         self.direction = None
         self.spritesheet = Spritesheet(spritesheet)
         self.scale = scale
         self.speed = speed
+        self.health = health
+        self.attack_power = attack_power
         self.last_update = pygame.time.get_ticks()
-        self.frame_rate = frame_rate;
+        self.frame_rate = frame_rate
         self.animations = {
             'idle': self.load_frames(frame_width, frame_height, 12, row=0),                 # Row 0: 12 frames for 'idle'
             'move_right': self.load_frames(64, 65, 8, row=1),            # Row 1: 8 frames for 'move_right'
             'move_left': self.load_frames(64, 65, 8, row=2),             # Row 2: 8 frames for 'move_left'
-            'jump': self.load_frames(64, 65, 4, row=3),                  # Row 3: 4 frames for 'jump'
-            'fall': self.load_frames(64, 65, 7, row=4),                  # Row 4: 7 frames for 'fall'
+            'move_up': self.load_frames(64, 65, 4, row=3),                  # Row 3: 4 frames for 'jump'
+            'move_down': self.load_frames(64, 65, 7, row=4),                  # Row 4: 7 frames for 'fall'
             'attack_right': self.load_frames(64, 65, 8, row=5),  # Add frame count for 'attack_right'
             'attack_left': self.load_frames(64, 65, 12, row=6),   # Add frame count for 'attack_left'
             'hurt': self.load_frames(64, 65, 8, row=7),          # Add frame count for 'hurt'
@@ -33,12 +37,13 @@ class Player(AnimatedSprite):
             'run_right': self.load_frames(64, 65, 12, row=12),    # Add frame count for 'run_right'
             'run_left': self.load_frames(64, 65, 8, row=13)      # Add frame count for 'run_left'
         }
-        self.current_animation = 'idle'
+        self.current_animation = 'hurt'
         self.frames = self.animations[self.current_animation]
         self.current_frame = 0
         self.image = self.frames[self.current_frame]
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
+        self.direction : string = None
 
     def update_animation(self, delta_time):
         now = pygame.time.get_ticks()
@@ -46,14 +51,30 @@ class Player(AnimatedSprite):
             self.last_update = now
             self.current_frame = (self.current_frame + 1) % len(self.frames)
             self.image = self.frames[self.current_frame]
+        if self.current_animation == 'hurt' and self.current_frame == len(self.frames) - 1:
+            self.set_animation('idle')
 
 
     def update(self, delta_time : float):
+        if self.direction == 'right':
+            self.rect.x += self.speed * delta_time
+            self.set_animation('move_right')
+        elif self.direction == 'left':
+            self.rect.x -= self.speed * delta_time
+            self.set_animation('move_left')
+        elif self.direction == 'up':
+            self.rect.y -= self.speed * delta_time
+            self.set_animation('move_up')
+        elif self.direction == 'down':
+            self.rect.y += self.speed * delta_time
+            self.set_animation('move_down')
+        else:
+            self.set_animation('idle')
+
         self.update_animation(delta_time)
 
     def get_position(self):
         return self.rect.center
-
 
     def load_frames(self, frame_width : int, frame_height : int, num_frames : int, row : int, flip=False) -> list:
         frames = []
@@ -66,14 +87,29 @@ class Player(AnimatedSprite):
             frames.append(frame)
         return frames
 
-    def set_animation_based_on_direction(self, direction):
-        if abs(direction.x) > abs(direction.y):
-            if direction.x > 0:
-                self.set_animation('right')
-            else:
-                self.set_animation('left')
-        else:
-            if direction.y > 0:
-                self.set_animation('idle')
-            else:
-                self.set_animation('back')
+    def set_animation(self, animation):
+        if animation in self.animations and self.current_animation != animation:
+            self.current_animation = animation
+            self.frames = self.animations[self.current_animation]
+            self.current_frame = 0
+
+    def take_damage(self, damage : int):
+        self.set_animation('hurt')
+        self.health -= damage
+
+    def move_right(self):
+        self.direction = 'right'
+
+    def move_left(self):
+        self.direction = 'left'
+
+    def move_down(self):
+        self.direction = 'down'
+
+    def move_up(self):
+        self.direction = 'up'
+
+    def stop(self):
+        self.direction = None
+
+    #def set_animation_based_on_direction(self, direction):
