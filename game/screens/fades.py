b2222972bfdfe70e21b97119ef499a857aeb74d7
Lucci228 <151786062+Lucci228@users.noreply.github.com>
2024-12-11 18:47:01 +0200

    Fixed camera issues thank god

--- game/screens/fades.py
+++ game/screens/fades.py
@@ -3,14 +3,11 @@ import pygame
 def fade_out(screen, width, height, tile_map, all_sprites, enemyList, player):
     fade = pygame.Surface((width, height))
     fade.fill((0, 0, 0))
+    screen.fill((0, 0, 0))
     for alpha in range(0, 255, 5):
         fade.set_alpha(alpha)
         screen.fill((0, 0, 0))
-        tile_map.render(screen)
-        all_sprites.draw(screen)
-        for enemy in enemyList:
-            if enemy.health_bar is not None:
-                enemy.health_bar.draw(screen)
+        all_sprites.draw(player.rect.center)
         player.healthBar.draw(screen)
         screen.blit(fade, (0, 0))
         pygame.display.update()
@@ -19,14 +16,11 @@ def fade_out(screen, width, height, tile_map, all_sprites, enemyList, player):
 def fade_in(screen, width, height, tile_map, all_sprites, enemyList, player):
     fade = pygame.Surface((width, height))
     fade.fill((0, 0, 0))
+    screen.fill((0, 0, 0))
     for alpha in range(255, 0, -5):
         fade.set_alpha(alpha)
         screen.fill((0, 0, 0))
-        tile_map.render(screen)
-        all_sprites.draw(screen)
-        for enemy in enemyList:
-            if enemy.health_bar is not None:
-                enemy.health_bar.draw(screen)
+        all_sprites.draw(player.rect.center)
         player.healthBar.draw(screen)
         screen.blit(fade, (0, 0))
         pygame.display.update()
